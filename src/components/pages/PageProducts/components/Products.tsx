eeeb8c688fe461db31fc6cf21f872b64e02b2e22
Priyanka <priyanka_terala@epam.com>
2022-07-15 17:01:22 +0530

    task-3 integrated api to get all products

--- src/components/pages/PageProducts/components/Products.tsx
+++ src/components/pages/PageProducts/components/Products.tsx
@@ -9,8 +9,8 @@ import {makeStyles} from '@material-ui/core/styles';
 import {Product} from "models/Product";
 import {formatAsPrice} from "utils/utils";
 import AddProductToCart from "components/AddProductToCart/AddProductToCart";
-// import axios from 'axios';
-// import API_PATHS from "constants/apiPaths";
+import axios from 'axios';
+import API_PATHS from "constants/apiPaths";
 import productList from "./productList.json";
 
 const useStyles = makeStyles((theme) => ({
@@ -42,8 +42,11 @@ export default function Products() {
   const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
-    // axios.get(`${API_PATHS.bff}/product/available/`)
-    //   .then(res => setProducts(res.data));
+    axios.get(`${API_PATHS.products}`)
+      .then(res => {
+        const products = res.data.products;
+        setProducts(products);
+      });
     setProducts(productList);
   }, [])
 

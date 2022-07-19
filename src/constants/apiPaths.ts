e1e7b19ac8d7e7f019e190703292f0ece3fc7b87
Priyanka <priyanka_terala@epam.com>
2022-07-19 13:45:09 +0530

    task-3 update api gateway link

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,6 +1,6 @@
 
 const API_PATHS = {
-  product: 'https://mje0gndf41.execute-api.us-east-1.amazonaws.com/dev',
+  product: 'https://bjuk3krz0k.execute-api.us-east-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
